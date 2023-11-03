c8477d27b3e86c61a870cb7926422f994830f255
Farrux Ro'zmetov <farrukhruzmetov2002@gmail.com>
2023-11-03 19:17:14 +0500

    aws database connected

--- DSCC_API/Program.cs
+++ DSCC_API/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-string connString = builder.Configuration.GetConnectionString("ConnString");
+string connString = builder.Configuration.GetConnectionString("BookStore");
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
